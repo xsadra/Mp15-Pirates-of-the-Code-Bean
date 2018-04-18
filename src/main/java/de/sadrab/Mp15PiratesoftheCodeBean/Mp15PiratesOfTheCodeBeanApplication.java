2324d3de600df6b7c77c629389ba9e6beabab945
Sadra <ti9mail@gmail.com>
2018-04-18 04:45:00 +0200

    add

---
+++ src/main/java/de/sadrab/Mp15PiratesoftheCodeBean/Mp15PiratesOfTheCodeBeanApplication.java
@@ -0,0 +1,11 @@
+package de.sadrab.Mp15PiratesoftheCodeBean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mp15PiratesOfTheCodeBeanApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Mp15PiratesOfTheCodeBeanApplication.class, args);
+    }
+}
