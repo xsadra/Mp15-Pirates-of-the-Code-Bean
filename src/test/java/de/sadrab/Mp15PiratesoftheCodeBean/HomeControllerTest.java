7172408d0d8ecbfb60184430d3b0d28f9f49613e
Sadra <ti9mail@gmail.com>
2018-04-20 10:05:30 +0200

    test

---
+++ src/test/java/de/sadrab/Mp15PiratesoftheCodeBean/HomeControllerTest.java
@@ -0,0 +1,18 @@
+package de.sadrab.Mp15PiratesoftheCodeBean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HomeControllerTest {
+
+    @org.junit.jupiter.api.Test
+    void result() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void page() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void choose() {
+    }
+}
